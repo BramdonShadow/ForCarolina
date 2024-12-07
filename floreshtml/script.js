1070ed8e452f31fb5effb460d208a838a4712407
BramdonShadow <69271896+BramdonShadow@users.noreply.github.com>
2024-12-07 16:03:40 -0500

    Add files via upload

---
+++ floreshtml/script.js
@@ -0,0 +1,7 @@
+onload = () => {
+  document.body.classList.remove("container");
+};
+window.alert("oa carolina :33 dale click a cualquier parte pa que suene la muscia OwO")
+  window.addEventListener('click', () => {
+  document.getElementById("musica").play();
+});
---
+++ js/music-list.js
@@ -0,0 +1,27 @@
+let allMusic = [
+  {
+    name: "Doshiyomonaikuraini kimigasuki",
+    artist: "Eve",
+    src: "Doshiyomonaikuraini-kimigasuki"
+  },
+  {
+    name: "Fuyukan",
+    artist: "Eve",
+    src: "Fuyukan"
+  },
+  {
+    name: "Outsider",
+    artist: "Eve",
+    src: "Outsider"
+  },
+  {
+    name: "Last Dance",
+    artist: "Eve",
+    src: "Last-Dance"
+  },
+  {
+    name: "Taikutsuwo Saienshinaide",
+    artist: "Eve",
+    src: "taikutsuwo-saienshinaide"
+  }
+];
